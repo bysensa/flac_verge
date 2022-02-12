1d0f3f18b216dd2b2739d171cb48b91b28a698b2
s-a-sen <s-a-sen@yandex-team.ru>
2022-02-12 23:31:48 +0300

    refactoring: change verge implementation

--- examples/personal_finance/android/app/src/main/kotlin/dev/bysensa/personal_finance/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.bysensa.personal_finance
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- examples/personal_finance/lib/state/application_store.dart
+++
@@ -1,7 +0,0 @@
-import 'package:verge/verge.dart';
-
-import 'application_state.dart';
-
-class ApplicationStore extends Store<ApplicationState> {
-  ApplicationStore() : super(ApplicationState.initial());
-}
